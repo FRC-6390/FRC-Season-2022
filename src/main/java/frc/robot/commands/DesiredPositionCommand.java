a2ff6d29a341b40aef8b7c30adf60f6c8b8a0a28
mohammadawwad <mohd-awwad@hotmail.com>
2022-02-02 18:48:47 -0800

    drive working, rough code from experimental branch

---
+++ src/main/java/frc/robot/commands/DesiredPositionCommand.java
@@ -0,0 +1,74 @@
+package frc.robot.commands;
+
+import java.util.Arrays;
+import java.util.Iterator;
+
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.wpilibj.shuffleboard.BuiltInLayouts;
+import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardLayout;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.drivetrain.DesiredPosition;
+import frc.robot.subsystems.drivetrain.SwerveDriveTrain;
+
+public class DesiredPositionCommand extends CommandBase {
+
+    private SwerveDriveTrain driveTrain;
+    private DesiredPosition currentCord;
+    private boolean done;
+    private ShuffleboardLayout waypointLayout = Shuffleboard.getTab("Auto").getLayout("Waypoint", BuiltInLayouts.kList).withSize(2, 8).withPosition(0, 0);
+    private ShuffleboardLayout drivelayout = Shuffleboard.getTab("Auto").getLayout("Drive PID", BuiltInLayouts.kList).withSize(2, 8).withPosition(2, 0);
+    private ShuffleboardLayout rotationlayout = Shuffleboard.getTab("Auto").getLayout("Rotation PID", BuiltInLayouts.kList).withSize(2, 8).withPosition(4, 0);
+    private static boolean shuffleboard = true;
+    private Iterator<DesiredPosition> cords;
+
+    public DesiredPositionCommand(SwerveDriveTrain subsystem, DesiredPosition... cords) {
+      driveTrain = subsystem;
+      this.cords = Arrays.asList(cords).iterator();
+      
+    }
+
+    private void setupShuffleboard(){
+      waypointLayout.addNumber("X", () -> currentCord.x());
+      waypointLayout.addNumber("Y", () -> currentCord.y());
+      waypointLayout.addNumber("Theta", () -> currentCord.theta());
+      waypointLayout.addBoolean("At Threshold", () -> currentCord.threashhold());
+      drivelayout.addNumber("P", () -> currentCord.getDrivePID().getP());
+      drivelayout.addNumber("I", () -> currentCord.getDrivePID().getI());
+      drivelayout.addNumber("D", () -> currentCord.getDrivePID().getD());
+      drivelayout.addNumber("Setpoint", () -> currentCord.getDrivePID().getSetpoint());
+      rotationlayout.addNumber("P", () -> currentCord.getRotationPID().getP());
+      rotationlayout.addNumber("I", () -> currentCord.getRotationPID().getI());
+      rotationlayout.addNumber("D", () -> currentCord.getRotationPID().getD());
+      rotationlayout.addNumber("Setpoint", () -> currentCord.getRotationPID().getSetpoint());
+      shuffleboard = false;
+    }
+  
+    @Override
+    public void initialize() {
+      currentCord = cords.next();
+      done = false;
+      // if(shuffleboard)setupShuffleboard();
+    }
+  
+    @Override
+    public void execute() {  
+        if(!currentCord.threashhold()){
+          driveTrain.drive(currentCord.getChassisSpeeds(driveTrain));
+        }else{
+          if (!cords.hasNext()) done = true;
+          else currentCord = cords.next();
+        }
+    }
+  
+    @Override
+    public void end(boolean interrupted) {}
+  
+    @Override
+    public boolean isFinished() {
+      return done;
+    }
+  }
+  
---
+++ src/main/java/frc/robot/commands/SwerveDriveCommand.java
@@ -0,0 +1,91 @@
+// package frc.robot.commands;
+
+// import frc.robot.Constants;
+// import frc.robot.subsystems.drivetrain.SwerveDriveTrain;
+// import edu.wpi.first.math.filter.SlewRateLimiter;
+// import edu.wpi.first.wpilibj.XboxController;
+// import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+// import edu.wpi.first.wpilibj2.command.CommandBase;
+
+// public class SwerveDrive extends CommandBase {
+
+//   private XboxController controller;
+//   private final SlewRateLimiter xSpeedLimiter = new SlewRateLimiter(3);
+//   private final SlewRateLimiter ySpeedLimiter = new SlewRateLimiter(3);
+//   private final SlewRateLimiter angularLimiter = new SlewRateLimiter(3);
+
+//   public SwerveDrive(XboxController controller) {
+//     this.controller = controller;
+//   }
+
+//   @Override
+//   public void initialize() {}
+
+//   @Override
+//   public void execute() {
+    
+//     double leftXAxis = xSpeedLimiter.calculate(inDeadZone(controller.getLeftY()) ? 0 : controller.getLeftY()) * Constants.SWERVE.PRECENT_MAX_SPEED;
+//     double leftYAxis = ySpeedLimiter.calculate(inDeadZone(controller.getLeftX()) ? 0 : controller.getLeftX()) * Constants.SWERVE.PRECENT_MAX_SPEED;
+
+//     double rightXAxis = angularLimiter.calculate(inDeadZone(controller.getRightX()) ? 0 : controller.getRightX()) * Constants.SWERVE.PRECENT_MAX_SPEED;
+
+//     SwerveDriveTrain.drive(leftXAxis * Constants.SWERVE.PRECENT_MAX_SPEED, leftYAxis * Constants.SWERVE.PRECENT_MAX_SPEED, rightXAxis * Constants.SWERVE.PRECENT_MAX_SPEED, true);
+//   }
+
+//   @Override
+//   public void end(boolean interrupted) {}
+
+//   @Override
+//   public boolean isFinished() {
+//     return false;
+//   }
+
+//   private boolean inDeadZone(double value){
+//     return !(value >= Constants.CONTROLLER.XBOX.DEAD_ZONE_MAX || value <= Constants.CONTROLLER.XBOX.DEAD_ZONE_MIN);
+//   }
+// }
+
+
+package frc.robot.commands;
+
+import java.lang.module.ModuleDescriptor.Requires;
+import java.util.function.DoubleSupplier;
+
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Robot;
+import frc.robot.subsystems.drivetrain.SwerveDriveTrain;
+
+public class SwerveDriveCommand extends CommandBase {
+
+  private SwerveDriveTrain driveTrain;
+  private DoubleSupplier x, y, r;
+  private ShuffleboardTab tab = Shuffleboard.getTab("Contoller");
+
+  public SwerveDriveCommand(SwerveDriveTrain subsystem, DoubleSupplier xSupplier, DoubleSupplier ySupplier, DoubleSupplier rSupplier) {
+    driveTrain = subsystem;
+    x = xSupplier;
+    y = ySupplier;
+    r = rSupplier;
+    addRequirements(subsystem);
+    tab.addNumber("X Axis", x);
+    tab.addNumber("Y Axis", y);
+    tab.addNumber("R Axis", r);
+  }
+
+  @Override
+  public void initialize() {}
+
+  @Override
+  public void execute() {
+    if(DriverStation.isTeleop())driveTrain.drive(ChassisSpeeds.fromFieldRelativeSpeeds(x.getAsDouble(),y.getAsDouble(),r.getAsDouble(),driveTrain.rotation()));
+  }
+
+  @Override
+  public void end(boolean interrupted) {
+    driveTrain.drive(new ChassisSpeeds(0.0, 0.0, 0.0));
+  }
+}
