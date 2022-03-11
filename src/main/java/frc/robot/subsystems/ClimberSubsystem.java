52aaac801a77812f4096eff3890e227153867374
Mathias <70431366+SoZ0@users.noreply.github.com>
2022-03-11 00:48:04 -0800

    proper blinkin support

--- src/main/java/frc/robot/subsystems/ClimberSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClimberSubsystem.java
@@ -8,7 +8,7 @@ import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import edu.wpi.first.util.sendable.SendableBuilder;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.ELEVATOR;
-import utilities.LimitSwitch;
+import utilities.sensorlib.LimitSwitch;
 
 public class ClimberSubsystem extends SubsystemBase {
 
