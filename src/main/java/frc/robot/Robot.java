fa3ba35497d7d8fcaf1f9f1b9349c7a22096fd46
mohammadawwad <mohd-awwad@hotmail.com>
2022-10-26 17:01:35 -0700

    commit

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -68,8 +68,8 @@ public class Robot extends TimedRobot {
 
   @Override
   public void teleopInit() {
-    ClimbArms.open();
-   // CommandScheduler.getInstance().schedule(new SystemsTest());
+    // ClimbArms.open();
+    CommandScheduler.getInstance().schedule(new SystemsTest());
     // CommandScheduler.getInstance().schedule(container.getDriveCommand());
     RobotContainer.driveTrain.reset(true);
   }
